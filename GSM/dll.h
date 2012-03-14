2ed16bfa89f73da3659ea7d125392291cefc91c2
Hans-Peter Gygax <gygax@practicomp.ch>
2012-03-14 01:13:08 +0100

    Now offering both DLL and static builds

--- GSM/dll.h
+++ GSM/dll.h
@@ -1,10 +1,14 @@
 #ifndef __GSM_DLL_H
 #define __GSM_DLL_H
 
+#ifdef GSM_STATIC
+#define GSM_API
+#else
 #ifdef GSM_EXPORTS
 #define GSM_API __declspec(dllexport)
 #else
 #define GSM_API __declspec(dllimport)
+#endif
 #endif
 
 #endif // __GSM_DLL_H
--- GSM/util/format.hpp
+++ GSM/util/format.hpp
@@ -15,10 +15,42 @@
 
 #include "..\dll.h"
 
-const std::string GSM_API
+const std::string
 format(const char *fmt, ...);
 
-const std::wstring GSM_API
+const std::wstring
 format(const wchar_t *fmt, ...);
+
+/*--- INLINE IMPLEMENTATIONS -------------------------------------------------*/
+
+inline const std::string
+format(const char *fmt, ...)
+{
+    va_list list;
+
+    va_start(list, fmt);
+
+    char buffer[2048+1];
+    vsnprintf(buffer, 2048, fmt, list);
+
+    va_end(list);
+
+    return std::string(buffer);
+}
+
+inline const std::wstring
+format(const wchar_t *fmt, ...)
+{
+    va_list list;
+
+    va_start(list, fmt);
+
+    wchar_t buffer[2048+1];
+    vswprintf(buffer, 2048, fmt, list);
+
+    va_end(list);
+
+    return std::wstring(buffer);
+}
 
 #endif // __GSM_UTIL_FORMAT_HPP
